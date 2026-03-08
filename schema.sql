b47aae82ee85bfca777686dbf2149cb4082c0c0d
Mathew Dony <49617008+mdmathewdc@users.noreply.github.com>
2026-03-08 23:51:01 +1100

    Remove INDEX as it might hamper recall performance if added on empty table: 2026-03-08

--- schema.sql
+++ schema.sql
@@ -6,5 +6,3 @@ CREATE TABLE IF NOT EXISTS journal_entries (
   created_at TIMESTAMP DEFAULT NOW(),
   embedding VECTOR(1536)
 );
-
-CREATE INDEX IF NOT EXISTS idx_journal_embedding ON journal_entries USING ivfflat (embedding vector_cosine_ops);
